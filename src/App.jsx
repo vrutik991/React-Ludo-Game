79c9dd9fbf316d76cf7d8dd6d589ab57b10485ed
Vrutik Rathod <96432653+vrutik991@users.noreply.github.com>
2025-01-18 16:09:48 +0530

    project completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,43 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [moves,setMoves] = useState({blue:0,yellow:0,red:0,green:0});
 
+  function updateBlue(){
+    setMoves((prevMoves)=>
+    {
+      return {...prevMoves, blue: prevMoves.blue + 1} // object is returned
+    });
+  }
+  function updateGreen(){
+    setMoves((prevMoves)=>
+    {
+      return {...prevMoves, green: prevMoves.green + 1} // object is returned
+    });
+  }
+  function updateYellow(){
+    setMoves((prevMoves)=>
+    {
+      return {...prevMoves, yellow: prevMoves.yellow + 1} // object is returned
+    });
+  }
+  function updateRed(){
+    setMoves((prevMoves)=>
+    {
+      return {...prevMoves, red: prevMoves.red + 1} // object is returned
+    });
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Ludo game begins!</h1>
+      <h3 >Blue moves = {moves.blue}</h3>
+      <button onClick={updateBlue}>+1</button>
+      <h3>Yellow moves = {moves.yellow}</h3>
+      <button onClick={updateYellow}>+1</button>
+      <h3>Red moves = {moves.red}</h3>
+      <button onClick={updateRed}>+1</button>
+      <h3>Green moves = {moves.green}</h3>
+      <button onClick={updateGreen}>+1</button>
     </>
   )
 }
